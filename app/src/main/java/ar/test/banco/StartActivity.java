8904bd11db8587f39c9690d23e6ccc7e5aa221ed
Mariano <marianogualtieri84@gmail.com>
2021-10-29 13:57:12 -0300

    inversiones

--- app/src/main/java/ar/test/banco/StartActivity.java
+++ app/src/main/java/ar/test/banco/StartActivity.java
@@ -1,22 +1,33 @@
 package ar.test.banco;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -30,9 +41,44 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+
         // conectar boton home
         home =  findViewById(R.id.home);
         close = findViewById(R.id.close);
+
+        Button cuenta = findViewById(R.id.btnCuenta);
+        Button tarjeta = findViewById(R.id.btnTarjeta);
+        Button inversion = findViewById(R.id.btnInversion);
+        EditText userName= findViewById(R.id.userName);
+
+
+
+        cuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment cuentaFragment = new Cuenta();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,cuentaFragment).commit();
+            }
+        });
+        tarjeta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment tarjetaFragment = new Tarjeta();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,tarjetaFragment).commit();
+
+
+            }
+        });
+        inversion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment inversionFragment = new Inversion();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,inversionFragment).commit();
+            }
+        });
 
 
         // assigning ID of the toolbar to a variable
@@ -41,41 +87,37 @@ public class StartActivity extends AppCompatActivity {
         // using toolbar as ActionBar
        setSupportActionBar(toolbar);
 
+
        // recuperar el dato desde login
-        String username = getIntent().getStringExtra("username");
-        Toast.makeText(getApplicationContext(), "Nombreen start es: " + username , Toast.LENGTH_SHORT).show();
+        SharedPreferences sh = getSharedPreferences("data", MODE_PRIVATE);
+        String nombre = sh.getString("name", "");
+        String apellido = sh.getString("lastname", "");
+        int pesos = sh.getInt("pesos",0);
+        int dolares = sh.getInt("dolares",0);
 
+        userName.setText(nombre);
 
-
-        Fragment fragment = new Fragment();
-        Bundle bundle = new Bundle();
-        bundle.putString("username",username);
-        fragment.setArguments(bundle);
 
         // poner a la escucha al boton home
         home.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
 
-
                FragmentContainer init = new FragmentContainer();
-               getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,init,username).commit();
-
-
-               // Navigation.findNavController(v).navigate(R.id.action_fragmentContainer_to_cuenta2);
-
+               getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,init).commit();
 
             }
+
             });
+
+
         close.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
                 AlertDialog alertDialog = new AlertDialog.Builder(StartActivity.this).create();
-                alertDialog.setTitle(username);
+                alertDialog.setTitle(nombre);
                 alertDialog.setMessage("Desea cerrar la sesion?");
 
                 alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "CANCEL",
@@ -97,12 +139,12 @@ public class StartActivity extends AppCompatActivity {
 
                 alertDialog.show();
 
-
                 // Navigation.findNavController(v).navigate(R.id.action_fragmentContainer_to_cuenta2);
 
 
             }
         });
+
 
     }
 }
